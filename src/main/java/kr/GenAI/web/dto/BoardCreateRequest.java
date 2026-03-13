01238a5e59fa41ba32910c46e882874eaee8317c
hyunhoAi <hyunho2212@gmail.congit config --global user.emailhyunho2212@gmail.con>
2026-03-13 10:35:47 +0900

    backend commit

---
+++ src/main/java/kr/GenAI/web/dto/BoardCreateRequest.java
@@ -0,0 +1,18 @@
+package kr.GenAI.web.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardCreateRequest {
+	private String b_title;
+	private String b_writer;
+	private String b_content;
+	private MultipartFile b_file;
+
+}
---
+++ src/test/java/kr/GenAI/web/BoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.GenAI.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
